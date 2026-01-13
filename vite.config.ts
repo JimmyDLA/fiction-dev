5e77b98db5f59b07b979acd2d7060b1721cbebd5
Jimmy DLA <jimmydelosangeles@gmail.com>
2026-01-12 23:41:38 -0400

    updated vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", 
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true, // Clears the dist folder before building
+  }
+});
