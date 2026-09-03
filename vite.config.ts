29b0d0d71e1fc51add685d76fa69f3874da4f93a
Jimmy DLA <jimmydelosangeles@gmail.com>
2026-09-03 17:05:13 -0400

    updated yml files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: process.env.BASE_PATH || (process.env.NODE_ENV === 'production' ? '/fiction-dev/' : '/'),
   server: {
     host: '::',
     port: 8080,
